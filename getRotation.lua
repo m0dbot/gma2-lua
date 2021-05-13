05dfd0b5fcb83995aeb35e0e219a8e21493d720f
Martin Strohmeier <59908941+m0dbot@users.noreply.github.com>
2021-05-13 23:32:28 +0200

    Update getRotation.lua

--- getRotation.lua
+++ getRotation.lua
@@ -1,8 +1,8 @@
- ---------------------------- getNextFreeSeq v1.0 ----------------------------
+ ----------------------------  getRotation  v1.0  ----------------------------
  --                   martin [at] klangbild [dot] lighting                  --
 
- -------- Script to set uservar nextFreeSeq to the next free sequence --------
- -- nextFreeSeq will be set to zero if nothing is free in the defined range --
+ --  Script to set uservars LS_rotX,LS_rotZ,LS_rotZ to the current patched  --
+ --  rotation of a fixture defined by uservar LS_ReadFixId                  --
  -----------------------------------------------------------------------------
  
 local input = gma.textinput;
@@ -12,17 +12,14 @@ local setvar = gma.user.setvar;
 local gmasleep = gma.sleep (0.1);
 local echo    =  gma.echo;
   
-  oldBPM = getvar("LS_D_BPM");
-  divi = getvar("LS_DIV");
-  
-  setvar("LS_D_BPM_DIV" , oldBPM / divi );
-  gmasleep();
+
  
 function getRotation()
  local FixId = getvar("LS_ReadFixId"); 
-	local rotX = gma.show.property.get(FixId,18);
-	local rotY = gma.show.property.get(FixId,19);
-	local rotZ = gma.show.property.get(FixId,20);
+ local rotX = gma.show.property.get(FixId,18);
+ local rotY = gma.show.property.get(FixId,19);
+ local rotZ = gma.show.property.get(FixId,20);
  echo('rotX : '..rotX' rotY : '..rotY' rotZ : '..rotZ);
- 
+ gmasleep();
+ setvar("LS_rotX" , rotX );
 end
