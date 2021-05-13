d5d0bb89933f6abbd1f0409c1fbefbf1ca324e90
Martin Strohmeier <59908941+m0dbot@users.noreply.github.com>
2021-05-14 01:30:08 +0200

    Update getRotation.lua

--- getRotation.lua
+++ getRotation.lua
@@ -1,31 +1,29 @@
- 
---NOT WORKING
-
-----------------------------  getRotation  v0.0  ----------------------------
+ ----------------------------  getRotation  v1.0  ----------------------------
  --                   martin [at] klangbild [dot] lighting                  --
+
  --  Script to set uservars LS_rotX,LS_rotZ,LS_rotZ to the current patched  --
  --  rotation of a fixture defined by uservar LS_ReadFixId                  --
  -----------------------------------------------------------------------------
  
 
-
- 
 function getRotation()
-gma.feedback("1");
-local grab  = gma.show.getobj
+
 local getvar = gma.user.getvar
 local setvar = gma.user.setvar
 local gmasleep = gma.sleep (0.1)  
-local FixId = 0
-local rotX = 1000
-local rotY = 1000
-local rotZ = 1000 
-FixId = getvar("LS_READFIXID")
-handle = gma.show.getobj.handle('fixture 'FixId);
-rotX = gma.show.property.get(handle,18);
-rotY = gma.show.property.get(handle,19);
-rotZ = gma.show.property.get(handle,20); 
-gma.feedback("rotX : " .. rotX .. " rotY : " .. rotY .. " rotZ : " .. rotZ);
+local FixId
+local rotX
+local rotY
+local rotZ
+
+FixId = getvar("LS_READFIXID");
+gma.feedback("reading fixture " .. FixId);
+local handle  =  gma.show.getobj.handle("Fixture " .. FixId);
+gma.feedback('FixtureType is '.. gma.show.property.get(handle,3));
+rotX = gma.show.property.get(handle,'RotX');
+rotY = gma.show.property.get(handle,'RotX');
+rotZ = gma.show.property.get(handle,'RotX'); 
+gma.feedback("X: " .. rotX .. " Y: " .. rotY .. " Z: " .. rotZ);
 gmasleep();
 setvar("LS_ROTX" , rotX);
 setvar("LS_ROTY" , rotY);
