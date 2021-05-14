ea68d0e96c3c4a5fa945a18855b3c069f512e7bf
Martin Strohmeier <59908941+m0dbot@users.noreply.github.com>
2021-05-14 02:38:18 +0200

    Update getRotation.lua

--- getRotation.lua
+++ getRotation.lua
@@ -1,8 +1,10 @@
- ----------------------------  getRotation  v1.0  ----------------------------
+ ----------------------------  getRotation  v1.1  ----------------------------
  --                   martin [at] klangbild [dot] lighting                  --
 
  --  Script to set uservars LS_rotX,LS_rotY,LS_rotZ to the current patched  --
  --  rotation of a fixture defined by uservar LS_ReadFixId                  --
+ -----------------------------------------------------------------------------
+ --  v1.1 - now also prints out the fixture's long name into Ls_FIXNAME     --
  -----------------------------------------------------------------------------
  
 
@@ -18,7 +20,8 @@ local rotZ
 FixId = getvar("LS_READFIXID");
 gma.feedback("reading fixture " .. FixId);
 local handle  =  gma.show.getobj.handle("Fixture " .. FixId .. ".1");
-gma.feedback('FixtureType is '.. gma.show.property.get(handle,3));
+local fixName = gma.show.property.get(handle,3);
+gma.feedback('FixtureType is '.. fixName);
 rotX = gma.show.property.get(handle,'RotX');
 rotY = gma.show.property.get(handle,'RotY');
 rotZ = gma.show.property.get(handle,'RotZ'); 
@@ -26,5 +29,6 @@ gma.feedback("X: " .. rotX .. " Y: " .. rotY .. " Z: " .. rotZ);
 setvar("LS_ROTX" , rotX);
 setvar("LS_ROTY" , rotY);
 setvar("LS_ROTZ" , rotZ);
+setvar("LS_FIXNAME" , fixName);
 end
 return getRotation;
