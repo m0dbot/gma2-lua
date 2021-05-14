bccc907f019279d5e041d08e7d3f84c365979f9d
Martin Strohmeier <59908941+m0dbot@users.noreply.github.com>
2021-05-14 04:09:19 +0200

    Update correctRotation.lua

--- correctRotation.lua
+++ correctRotation.lua
@@ -1,4 +1,4 @@
- --------------------------  correctRotation  v0.3  --------------------------
+ -------------------------  correctRotation  v0.42  --------------------------
  --                   martin [at] klangbild [dot] lighting                  --
 
  -----------------------------------------------------------------------------
@@ -12,7 +12,8 @@ local endFix = 99                 -- last fixture to mofify
 
 local getvar = gma.user.getvar    -- gma.user.setvar or gma.system.getvar
 local cmd    =  gma.cmd 
-local FixId
+local sleep   =  gma.sleep
+local fixId
 local rotX
 local rotY
 local rotZ
@@ -24,19 +25,20 @@ local modRotY
 local modRotZ
 local found
 
-FixId = getvar("LS_READFIXID")
+
+for fixId = startFix, endFix do
 
 
-gma.feedback("reading fixture " .. FixId)
-local handle  =  gma.show.getobj.handle("Fixture " .. FixId .. ".1")
+gma.feedback("reading fixture " .. fixId)
+local handle  =  gma.show.getobj.handle("Fixture " .. fixId .. ".1")
 local fixName = gma.show.property.get(handle,3)
 gma.feedback('FixtureType is ' .. fixName)
 found = 1
 
 if string.match(fixName, "Pointe Mode 3") then
    modRotX = 0
-   modRotY = 90
-   modRotZ = 180
+   modRotY = -90
+   modRotZ = 1
   
 elseif string.match(fixName, "Dimmer 00") then
    modRotX = 0
@@ -66,10 +68,17 @@ if found == 1 then
   rotX = gma.show.property.get(handle,'RotX')
   rotY = gma.show.property.get(handle,'RotY')
   rotZ = gma.show.property.get(handle,'RotZ')
+  
+  
+  gma.feedback("X: " .. rotX .. " Y: " .. rotY .. " Z: " .. rotZ)
 
   newRotX = rotX + modRotX
   newRotY = rotY + modRotY
   newRotZ = rotZ + modRotZ
+  
+  
+  gma.feedback("mod X: " .. modRotX .. " Y: " .. modRotY .. " Z: " .. modRotZ)
+  gma.feedback("new X: " .. newRotX .. " Y: " .. newRotY .. " Z: " .. newRotZ)
   
   if newRotX > 180 then
     newRotX = newRotX - 360
@@ -85,12 +94,17 @@ if found == 1 then
     newRotZ = newRotZ - 360
   elseif newRotZ < -180 then
     newRotZ = newRotZ + 360
-  end	
+  end    
+  
 
-  cmd('Fixture ' .. FixId)
-  --gma.sleep 0.01
-  cmd('Rotate3D At '..newRotZ..' '..newRotZ..' '..newRotZ)
-  --gma.sleep 0.01
+  gma.feedback("cor X: " .. newRotX .. " Y: " .. newRotY .. " Z: " .. newRotZ)
+
+  cmd('Fixture ' .. fixId)
+  sleep(0.1)
+  cmd('Rotate3D At ' .. newRotX .. ' ' .. newRotY .. ' ' .. newRotZ)
+  sleep(0.1)
+
+end
 
 end
 
